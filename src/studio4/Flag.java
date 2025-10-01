e75f0c6a879f4c788d384dcb84cd7b9c3aaf3feb
25WilliamsJ <jackson.p.williams@wustl.edu>
2025-10-01 15:39:56 -0500

    Studio 4 Complete#

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -6,6 +6,10 @@ import edu.princeton.cs.introcs.StdDraw;
 
 public class Flag {
 	public static void main(String[] args) {
-		
+		StdDraw.setPenColor(0,0,128);
+		StdDraw.filledRectangle(.5,.5,.4,.25);
+		StdDraw.setPenColor(StdDraw.RED);
+		StdDraw.filledRectangle(.5,.5,.04,.15);
+		StdDraw.filledRectangle(.5,.5,.15,.04);
 	}
 }
--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -19,6 +19,40 @@ public class InterpretDrawingFile {
 		chooser.showOpenDialog(null);
 		File f = new File(chooser.getSelectedFile().getPath());
 		Scanner in = new Scanner(f); //making Scanner with a File
-		
+		String shapeType = in.next();
+		int redComponent = in.nextInt();
+		int greenComponent = in.nextInt();
+		int blueComponent = in.nextInt();
+		boolean isFilled = in.nextBoolean();
+		double parameterOne = in.nextDouble();
+		double parameterTwo = in.nextDouble();
+		double parameterThree = in.nextDouble();
+		double parameterFour = in.nextDouble();
+		StdDraw.setPenColor(redComponent,greenComponent,blueComponent);
+		if (isFilled==true){
+			if (shapeType.equals("ellipse")) {
+				StdDraw.filledEllipse(parameterOne,parameterTwo,parameterThree,parameterFour);
+			} else if (shapeType.equals("rectangle")) {
+				StdDraw.filledRectangle(parameterOne,parameterTwo,parameterThree,parameterFour);
+			} else {
+				double parameterFive = in.nextDouble();
+				double parameterSix = in.nextDouble();
+				double[] x = {parameterOne,parameterThree,parameterFive};
+				double[] y = {parameterTwo,parameterFour,parameterSix};
+				StdDraw.filledPolygon(x,y);
+			}
+		} else {
+			if (shapeType.equals("ellipse")) {
+				StdDraw.ellipse(parameterOne,parameterTwo,parameterThree,parameterFour);
+			} else if (shapeType.equals("rectangle")) {
+				StdDraw.rectangle(parameterOne,parameterTwo,parameterThree,parameterFour);
+			} else {
+				double parameterFive = in.nextDouble();
+				double parameterSix = in.nextDouble();
+				double[] x = {parameterOne,parameterThree,parameterFive};
+				double[] y = {parameterTwo,parameterFour,parameterSix};
+				StdDraw.polygon(x,y);
+			}
+		}
 	}
 }
